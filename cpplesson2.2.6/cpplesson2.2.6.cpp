65e887d4a4296b0a3d1d5f7a4bdf5e49e6ace59c
VladislavErekeshov <tommysyching@gmail.com>
2021-02-09 20:27:16 +0300

    Добавьте файлы проекта.

---
+++ cpplesson2.2.6/cpplesson2.2.6.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a * 100 + b * 10 + c << endl;
+    cout << a * 100 + c * 10 + b << endl;
+    cout << b * 100 + a * 10 + c << endl;
+    cout << b * 100 + c * 10 + a << endl;
+    cout << c * 100 + a * 10 + b << endl;
+    cout << c * 100 + b * 10 + a << endl;
+    return 0;
+}
